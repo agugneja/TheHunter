65226a6cda2a93574638e7ac2fee888ae80e4c31
agugneja <99939910+agugneja@users.noreply.github.com>
2023-04-21 17:57:24 -0400

    added animation on attack, fixed some minor bugs

--- Assets/Scripts/PlayerDamage.cs
+++ Assets/Scripts/PlayerDamage.cs
@@ -7,20 +7,39 @@ public class PlayerDamage : MonoBehaviour
 {
     public float maxHealth = 1f;
     public float currentHealth = 1f;
+    bool canCauseDamage = true;
 
-    private void OnCollisionEnter(Collision other) {
-        if(other.collider.gameObject.CompareTag("Mutant")) {
-           currentHealth -= .2f;
+    private void OnTriggerStay(Collider other) {
+        if(other.GetComponent<Collider>().gameObject.CompareTag("Mutant")) {
+           StartCoroutine(Damage());
+           Debug.Log(currentHealth);
         }
     }
 
     void checkDeath() {
-        if(currentHealth <= 0) {
+        if(currentHealth <= 0.001f) {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
             SceneManager.LoadScene(3);
         }
     }
 
     void Update() {
         checkDeath();
+    }
+
+    IEnumerator Damage() 
+    {
+        if(canCauseDamage) {
+            canCauseDamage = false;            
+            currentHealth -= .2f;
+
+            yield return new WaitForSeconds(2.7f);
+            
+            canCauseDamage = true;
+        }
+        
+
+
     }
 }
