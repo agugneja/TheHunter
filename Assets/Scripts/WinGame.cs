74b1607ec3d575932a8089288eb36be024ca70c0
agugneja <99939910+agugneja@users.noreply.github.com>
2023-04-29 15:39:44 -0400

    minor bug fix

--- Assets/Scripts/WinGame.cs
+++ Assets/Scripts/WinGame.cs
@@ -10,6 +10,8 @@ public class WinGame : MonoBehaviour
         //if the power is on and player touches exit fence trigger game win
         int powerCheck = PlayerPrefs.GetInt("PowerOn");
         if(powerCheck == 1 && other.gameObject.tag == "ExitFence") {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
             SceneManager.LoadScene(2);
         }
     }
